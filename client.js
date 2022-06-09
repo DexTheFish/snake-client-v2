072e5bbbe47686ef3c75fb3be361fc9440819d50
Declan Davis <dexthefish@gmail.com>
2022-06-08 20:08:18 -0400

    wrapped mischievious behaviour into blocks that can be easily toggled with the 'trolling' boolean.

--- client.js
+++ client.js
@@ -1,3 +1,16 @@
+const net = require("net");
+const PORT = 50541;
+const banter = ['DEX','DEX','DEX','DEX','DEX','DEX','DEX','DEX',
+ 'THE','THE','THE','THE','THE','THE','THE','THE',
+  'FISH','FISH','FISH','FISH','FISH','FISH','FISH','FISH',
+'hit me I dare ya','hit me I dare ya','hit me I dare ya','hit me I dare ya',
+'hit me I dare ya','hit me I dare ya','hit me I dare ya','hit me I dare ya',
+'hit me I dare ya','hit me I dare ya','hit me I dare ya','hit me I dare ya',
+'hit me I dare ya','hit me I dare ya','hit me I dare ya','hit me I dare ya'];
+const directions = ['up', 'right', 'up', 'right', 'right','right', 'down', 'right', 'down', 'down',
+'left', 'down', 'left', 'left', 'left', 'up', 'left', 'up'];
+const trolling = true;
+
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
@@ -8,20 +21,21 @@ const connect = function () {
   // event handlers:
   conn.on("connect", () => {
     console.log('Connected!');
-    conn.write('Name: DD');
   });
   conn.on("data", (data) => {
     console.log(data);
-    connect(); // <- TROLLING
+    if (trolling) {
+      connect();
+    }
   });
-  /////TROLLING////////
-  for (let i = 0; i < 50000; i++) {
-    setTimeout(() => {
-      conn.write(`Say: ${banter[i % 32]}`);
-      conn.write(`Move: ${directions[i % 8]}`);
-    }, i * 100);
+  if (trolling) {
+    for (let i = 0; i < 50000; i++) {
+      setTimeout(() => {
+        conn.write(`Say: ${banter[i % banter.length]}`);
+        conn.write(`Move: ${directions[i % directions.length]}`);
+      }, i * 100);
+    }
   }
-  ////// DONE TROLLING/////////
   return conn;
 };
 module.exports = { connect };
