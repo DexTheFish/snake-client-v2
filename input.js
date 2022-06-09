b8b98431feaf93d829236de9c87175f4fa709e71
Declan Davis <dexthefish@gmail.com>
2022-06-08 20:07:04 -0400

    created input.js to set up and handle user input. Began working on functionality.

---
+++ input.js
@@ -0,0 +1,16 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  return;
+}
+
+module.exports = { setupInput, handleUserInput};
